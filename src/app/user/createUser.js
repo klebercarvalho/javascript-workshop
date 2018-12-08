d30cb0acdaccdc9915696d1b9601f78e02cc1534
Kleber Carvalho <kleber.carvalho@gmail.com>
2018-12-08 13:57:02 -0200

    Update before the lunch time

---
+++ src/app/user/createUser.js
@@ -0,0 +1,26 @@
+const User = require('../../domain/user/User')
+// caso de uso
+// criar os testes deve ser tão facil quanto a controller
+// e nesse caso de uso de deve fica dentro dele as coisas
+const createUser = ({ userRepository }) => {
+  // usando funcao para evitar boilerplate
+  return async function (userData, {
+    onSuccess,
+    onInValidUser,
+    onError
+  }) {
+    const user = new User(userData)
+    const validation = user.validate()
+    if (!validation.isValid) {
+      return onInValidUser(validation.error)
+    }
+    try {
+      const newUser = await userRepository.add(user)
+      return onSuccess(newUser)
+    } catch (error) {
+      return onError(error)
+    }
+  }
+}
+// exporta uma funcao que cria o usuario, padrao factory
+module.exports = createUser
